5415607b86a6680350be2863546eb4cc8b560609
hoge <hoge@hoge.com>
2018-06-19 15:08:49 +0900

    component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 
-class App extends Component {
-  render() {
-    return(
-      <div>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onClick={() => {console.log("I'm Clicked.")}} />;
-      </div>
-      )
-  }
+//class App extends Component {
+//  render() {
+//    return(
+//      <div>
+//      <label htmlFor="bar">bar</label>
+//      <input type="text" onClick={() => {console.log("I'm Clicked.")}} />;
+//      </div>
+//      )
+//  }
+//}
+//
+
+var App = () => {
+  return <div>Hi!</div>;
 }
 
 export default App;
